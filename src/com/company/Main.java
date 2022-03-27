91d252e302854239422ba2bfc26f48875f1338a7
derli <derlis.sanchez@sisb.edu.ec>
2022-03-27 15:45:49 -0400

    introducciom

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int resultado;
+        int suma= suma(1,3,2);
+        System.out.println("a+b+c");
+    }
+    public static int suma(int a, int b, int c){
+        return a+b+c;
+    };
+}
